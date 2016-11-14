c44f3c2eee87e819fb08038eba5e57b4fb2deb1a
Chitra Singh <chits.26@gmail.com>
2016-11-14 15:31:27 +0100

    Update: bounding box

--- rt/bbox.cpp
+++ rt/bbox.cpp
@@ -1,23 +1,30 @@
-#include "bbox.h"
+#include <rt/bbox.h>
 #include <core/assert.h>
+
+float maxFloat = std::numeric_limits<float>::max();
 
 namespace rt
 {
+	// BBox(const BBox &bbox)
+	// {
+
+	// }
 	BBox BBox::empty()
 	{
-		NOT_IMPLEMENTED;
+		return BBox::BBox(0.0, 0.0);
 	}
 	BBox BBox::full()
 	{
-		NOT_IMPLEMENTED;
+		return BBox::BBox(maxFloat, maxFloat);
 	}
 	void BBox::extend(const Point & point)
 	{
-		NOT_IMPLEMENTED;
+		return BBox::BBox(min(min(this->min, point), this->max), max(max(this->min, point), this->max));
 	}
 	void BBox::extend(const BBox & bbox)
 	{
-		NOT_IMPLEMENTED;
+		this->max = max(this->max, bbox.max); 
+		this->min = min(this->min, bbox.min);
 	}
 	std::pair<float, float> BBox::intersect(const Ray & ray) const
 	{
