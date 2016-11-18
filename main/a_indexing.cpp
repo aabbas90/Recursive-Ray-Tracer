4accdac26e926ac2e02fad308560a3b72a64dfbd
Chitra Singh <chits.26@gmail.com>
2016-11-18 02:27:42 +0100

    Update: iterative tree traversal and intersection, according to slides - infinite loop

--- main/a_indexing.cpp
+++ main/a_indexing.cpp
@@ -20,39 +20,41 @@ void a_indexing() {
 	Image img(800, 600);
 	
 	BVH* scene = new BVH();
-	scene->add(new Sphere(Point(2.5f,  .5f,  -1), 0.5  , nullptr, nullptr));
-	scene->add(new Sphere(Point(2.5f,  -1.f,  -1), 0.5, nullptr, nullptr));
-	scene->add(new Sphere(Point(4.5f,  .5f,  -1), 0.5 , nullptr, nullptr));
+	// scene->add(new Sphere(Point(2.5f,  .5f,  -1), 0.5  , nullptr, nullptr));
+	// scene->add(new Sphere(Point(2.5f,  -1.f,  -1), 0.5, nullptr, nullptr));
+	// scene->add(new Sphere(Point(4.5f,  .5f,  -1), 0.5 , nullptr, nullptr));
 
-	loadOBJ(scene, "models/", "cow.obj");
+	//loadOBJ(scene, "models/", "cow.obj");
   
 	// Uncomment after BVH:
+	// scene->rebuildIndex();
+	// World world;
+	// world.scene = scene;
+
+	// PerspectiveCamera cam1(Point(-8.85f, -7.85f, 7.0f), Vector(1.0f,1.0f,-0.6f), Vector(0, 0, 1), pi/8, pi/6);
+	// PerspectiveCamera cam2(Point(16.065f, -12.506f, 1.771f), Point(-0.286f, -0.107f, 1.35f)-Point(16.065f, -12.506f, 1.771f), Vector(0, 0, 1), pi/8, pi/6);
+	// RayCastingIntegrator integrator(&world);
+
+	// Renderer engine1(&cam1, &integrator);
+	// engine1.render(img);
+	// img.writePNG("a3-1.png");
+
+	// Renderer engine2(&cam2, &integrator);
+	// engine2.render(img);
+	// img.writePNG("a3-2.png");
+
+
+	loadOBJ(scene, "models/", "cow.obj");
+	// scene->add(new Sphere(Point(-2.2f, 1.7f, 0), 2, nullptr, nullptr));
+	// scene->add(new Sphere(Point(1, -1, 1), 2.2f, nullptr, nullptr));
+	// scene->add(new Sphere(Point(3.f, 0.8f, -2), 2, nullptr, nullptr));
+
+	// scene->add(new Triangle(Point(-2.01, 3.7f, 0), Point(1, 2, 1), Point(3, 2.8f, -2), nullptr, nullptr));
+	// scene->add(new Triangle(Point(3, 2, 3), Point(3, 2, -3), Point(-3, 2, -3), nullptr, nullptr));
+
+	// scene->add(new AABox(Point(2.01, 1.5f, -0.5f), Point(3, 2.5f, 2.5f), nullptr, nullptr));
 	scene->rebuildIndex();
-	World world;
-	world.scene = scene;
-
-	PerspectiveCamera cam1(Point(-8.85f, -7.85f, 7.0f), Vector(1.0f,1.0f,-0.6f), Vector(0, 0, 1), pi/8, pi/6);
-	PerspectiveCamera cam2(Point(16.065f, -12.506f, 1.771f), Point(-0.286f, -0.107f, 1.35f)-Point(16.065f, -12.506f, 1.771f), Vector(0, 0, 1), pi/8, pi/6);
-	RayCastingIntegrator integrator(&world);
-
-	Renderer engine1(&cam1, &integrator);
-	engine1.render(img);
-	img.writePNG("a3-1.png");
-
-	Renderer engine2(&cam2, &integrator);
-	engine2.render(img);
-	img.writePNG("a3-2.png");
-
-
-/*	scene->add(new Sphere(Point(-2.2f, 1.7f, 0), 2, nullptr, nullptr));
-	scene->add(new Sphere(Point(1, -1, 1), 2.2f, nullptr, nullptr));
-	scene->add(new Sphere(Point(3.f, 0.8f, -2), 2, nullptr, nullptr));
-
-	scene->add(new Triangle(Point(-2.01, 3.7f, 0), Point(1, 2, 1), Point(3, 2.8f, -2), nullptr, nullptr));
-	scene->add(new Triangle(Point(3, 2, 3), Point(3, 2, -3), Point(-3, 2, -3), nullptr, nullptr));
-
-	scene->add(new AABox(Point(2.01, 1.5f, -0.5f), Point(3, 2.5f, 2.5f), nullptr, nullptr));
-	scene->rebuildIndex();
+	
 
 	World world;
 	world.scene = scene;
@@ -61,5 +63,5 @@ void a_indexing() {
 	RayCastingIntegrator integrator(&world);
 	Renderer engine(&cam, &integrator);
 	engine.render(img);
-	img.writePNG("a3-1.png")*/;
+	img.writePNG("a3-1.png");
 }
