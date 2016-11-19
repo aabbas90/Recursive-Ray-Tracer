0cc5d3a6a13b54d26e4f3b01c2b5390d1c32073f
Ahmed <ahmed.a.mirza@hotmail.com>
2016-11-19 13:16:36 +0100

    Added stopwatch.

--- main/a_indexing.cpp
+++ main/a_indexing.cpp
@@ -11,8 +11,49 @@
 #include <rt/solids/sphere.h>
 #include <rt/cameras/perspective.h>
 #include <rt/integrators/casting.h>
-
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <sys/time.h>
+#include <ctime>
+#endif
 using namespace rt;
+typedef long long int64; typedef unsigned long long uint64;
+
+uint64 GetTimeMs64()
+{
+#ifdef _WIN32
+	/* Windows */
+	FILETIME ft;
+	LARGE_INTEGER li;
+
+	/* Get the amount of 100 nano seconds intervals elapsed since January 1, 1601 (UTC) and copy it
+	* to a LARGE_INTEGER structure. */
+	GetSystemTimeAsFileTime(&ft);
+	li.LowPart = ft.dwLowDateTime;
+	li.HighPart = ft.dwHighDateTime;
+
+	uint64 ret = li.QuadPart;
+	ret -= 116444736000000000LL; /* Convert from file time to UNIX epoch time. */
+	ret /= 10000; /* From 100 nano seconds (10^-7) to 1 millisecond (10^-3) intervals */
+
+	return ret;
+#else
+	/* Linux */
+	struct timeval tv;
+
+	gettimeofday(&tv, NULL);
+
+	uint64 ret = tv.tv_usec;
+	/* Convert from micro seconds (10^-6) to milliseconds (10^-3) */
+	ret /= 1000;
+
+	/* Adds the seconds (10^0) after converting them to milliseconds (10^-3) */
+	ret += (tv.tv_sec * 1000);
+
+	return ret;
+#endif
+}
 
 void a_indexing() {
     Image img(800, 600);
@@ -25,7 +66,10 @@ void a_indexing() {
 
 	loadOBJ(scene, "models/", "cow.obj");
   
+	auto startTime = GetTimeMs64();
 	scene->rebuildIndex();
+	auto endTime = GetTimeMs64();
+	std::cout << "Tree building time(ms): " << endTime - startTime << std::endl;
     World world;
     world.scene = scene;
 
@@ -34,10 +78,16 @@ void a_indexing() {
     RayCastingIntegrator integrator(&world);
 
     Renderer engine1(&cam1, &integrator);
+	startTime = GetTimeMs64();
     engine1.render(img);
+	endTime = GetTimeMs64();
+	std::cout << "Image 1 rendering time(ms): " << endTime - startTime << std::endl;
     img.writePNG("a3-1.png");
 
     Renderer engine2(&cam2, &integrator);
+	startTime = GetTimeMs64();
     engine2.render(img);
+	endTime = GetTimeMs64();
+	std::cout << "Image 2 rendering time(ms): " << endTime - startTime << std::endl;
     img.writePNG("a3-2.png");
 }
