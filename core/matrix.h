f6db413002cd9176b9ac2e0ea5a43fa70618e09d
Ahmed <ahmed.a.mirza@hotmail.com>
2016-11-24 16:50:53 +0100

    Adding matrix.cpp (uncomplete)

--- core/matrix.h
+++ core/matrix.h
@@ -7,32 +7,35 @@ namespace rt {
 
 class Matrix {
 public:
-    Matrix() {}
-    Matrix(const Float4& r1, const Float4& r2, const Float4& r3, const Float4& r4);
+	Matrix() {}
+	Matrix(const Float4& r1, const Float4& r2, const Float4& r3, const Float4& r4);
 
-    Float4& operator[](int idx);
-    Float4 operator[](int idx) const;
+	Float4& operator[](int idx);
+	Float4 operator[](int idx) const;
 
-    Matrix operator+(const Matrix& b) const;
-    Matrix operator-(const Matrix& b) const;
-    
-    Matrix transpose() const;
-    Matrix invert() const;
+	Matrix operator+(const Matrix& b) const;
+	Matrix operator-(const Matrix& b) const;
+	
+	Matrix transpose() const;
+	Matrix invert() const;
 
-    bool operator==(const Matrix& b) const;
-    bool operator!=(const Matrix& b) const;
+	bool operator==(const Matrix& b) const;
+	bool operator!=(const Matrix& b) const;
 
 
-    Float4 operator*(const Float4& b) const;
-    Vector operator*(const Vector& b) const;
-    Point operator*(const Point& b) const;
+	Float4 operator*(const Float4& b) const;
+	Vector operator*(const Vector& b) const;
+	Point operator*(const Point& b) const;
 
-    float det() const;
+	float det() const;
 
-    static Matrix zero();
-    static Matrix identity();
+	static Matrix zero();
+	static Matrix identity();
 
-    static Matrix system(const Vector& e1, const Vector& e2, const Vector& e3);
+	static Matrix system(const Vector& e1, const Vector& e2, const Vector& e3);
+
+private:
+	Float4 r1, r2, r3, r4;
 };
 
 Matrix product(const Matrix& a, const Matrix& b);
