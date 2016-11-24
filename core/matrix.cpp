6525c2b5b2ff1f157a59a322f49d8fb2784ce2a2
Ahmed <ahmed.a.mirza@hotmail.com>
2016-11-24 18:13:07 +0100

    Completed float4 and matrix.

--- core/matrix.cpp
+++ core/matrix.cpp
@@ -123,10 +123,29 @@ namespace rt {
 
 	}
 
-	//Vector Matrix::operator*(const Vector & b) const
-	//{
-	//	return Vector();
-	//}
+	Vector Matrix::operator*(const Vector & b) const
+	{
+		return this->operator*(Float4(b)).ToVector();
+	}
+
+	Point Matrix::operator*(const Point & b) const
+	{
+		return this->operator*(Float4(b)).ToPoint();
+	}
+
+	float Matrix::det() const
+	{
+		Matrix result;
+		const Matrix& m = *this;
+
+		//Taken and modified from http://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
+		result[0][0] = m[1][1] * m[2][2] * m[3][3] - m[1][1] * m[2][3] * m[3][2] - m[2][1] * m[1][2] * m[3][3] + m[2][1] * m[1][3] * m[3][2] + m[3][1] * m[1][2] * m[2][3] - m[3][1] * m[1][3] * m[2][2];
+		result[1][0] = -m[1][0] * m[2][2] * m[3][3] + m[1][0] * m[2][3] * m[3][2] + m[2][0] * m[1][2] * m[3][3] - m[2][0] * m[1][3] * m[3][2] - m[3][0] * m[1][2] * m[2][3] + m[3][0] * m[1][3] * m[2][2];
+		result[2][0] = m[1][0] * m[2][1] * m[3][3] - m[1][0] * m[2][3] * m[3][1] - m[2][0] * m[1][1] * m[3][3] + m[2][0] * m[1][3] * m[3][1] + m[3][0] * m[1][1] * m[2][3] - m[3][0] * m[1][3] * m[2][1];
+		result[3][0] = -m[1][0] * m[2][1] * m[3][2] + m[1][0] * m[2][2] * m[3][1] + m[2][0] * m[1][1] * m[3][2] - m[2][0] * m[1][2] * m[3][1] - m[3][0] * m[1][1] * m[2][2] + m[3][0] * m[1][2] * m[2][1];
+
+		return m[0][0] * result[0][0] + m[0][1] * result[1][0] + m[0][2] * result[2][0] + m[0][3] * result[3][0];
+	}
 
 	Matrix Matrix::zero()
 	{
