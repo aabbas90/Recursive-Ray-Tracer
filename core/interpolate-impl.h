363fe8bc08ce848b06a748d1253777610124fc95
Chitra Singh <chits.26@gmail.com>
2016-12-20 11:41:12 +0100

    Update: lerp and lerp 3d

--- core/interpolate-impl.h
+++ core/interpolate-impl.h
@@ -5,19 +5,18 @@ namespace rt {
 
     template <typename T>
     T lerp(const T& px0, const T& px1, float xPoint) {
-		//TODO
-        NOT_IMPLEMENTED;
+        //T *float is allowed
+		return ( px0 * (1 - xPoint)  + px1 * xPoint );
     }
 
     template <typename T>
     T lerpbar(const T& a, const T& b, const T& c, float aWeight, float bWeight) {
-		//TODO
-        NOT_IMPLEMENTED;
+		return (a * aWeight + b * bWeight + c * (1 - (aWeight + bWeight));
     }
 
     template <typename T>
     T lerp2d(const T& px0y0, const T& px1y0, const T& px0y1, const T& px1y1, float xWeight, float yWeight) {
-		//TODO
+        //TODO
         NOT_IMPLEMENTED;
     }
 
@@ -25,8 +24,15 @@ namespace rt {
     T lerp3d(const T& px0y0z0, const T& px1y0z0, const T& px0y1z0, const T& px1y1z0,
         const T& px0y0z1, const T& px1y0z1, const T& px0y1z1, const T& px1y1z1,
         float xPoint, float yPoint, float zPoint) {
-			//TODO
-            NOT_IMPLEMENTED;
+			T c00 = px0y0z0 * (1 - xPoint) + px1y0z0 * xPoint;
+			T c01 = px0y0z1 * (1 - xPoint) + px1y0z1 * xPoint;
+			T c10 = px0y1z0 * (1 - xPoint) + px1y1z0 * xPoint;
+			T c11 = px0y1z1 * (1 - xPoint) + px1y1z1 * xPoint;
+            
+            T c0 = c00 * (1 - yPoint) + c10 * yPoint;
+            T c1 = c01 * (1 - yPoint) + c11 * yPoint; 
+
+            return c0 * (1 - zPoint) + c1 * zPoint;
     }
 
 }
