c8388a7c34eb03afeda56e3952860115d46b0ecf
Chitra Singh <chits.26@gmail.com>
2016-12-23 13:16:41 +0100

    add: flat material

--- rt/materials/flatmaterial.cpp
+++ rt/materials/flatmaterial.cpp
@@ -1,8 +1,27 @@
-#include "flatmaterial.h"
+#include <rt/materials/flatmaterial.h>
+#include <core/color.h>
+
 
 namespace rt
 {
 	FlatMaterial::FlatMaterial(Texture * texture)
 	{
+		this->texture = texture;
 	}
+	RGBColor FlatMaterial::getReflectance(const Point& texPoint, const Vector& normal, const Vector& outDir, const Vector& inDir) const
+	{
+		//no reflectance
+		return RGBColor::black();
+	}
+	
+    RGBColor FlatMaterial::getEmission(const Point& texPoint, const Vector& normal, const Vector& outDir) const
+	{
+		return texture->getColor(texPoint);
+	}
+
+    SampleReflectance FlatMaterial::getSampleReflectance(const Point& texPoint, const Vector& normal, const Vector& outDir) const
+	{
+		return SampleReflectance();
+	}
+
 }
--- rt/materials/flatmaterial.h
+++ rt/materials/flatmaterial.h
@@ -13,6 +13,8 @@ public:
     virtual RGBColor getEmission(const Point& texPoint, const Vector& normal, const Vector& outDir) const;
     virtual SampleReflectance getSampleReflectance(const Point& texPoint, const Vector& normal, const Vector& outDir) const;
     virtual Sampling useSampling() const { return Material::SAMPLING_NOT_NEEDED; }
+private:
+    Texture* texture;
 };
 
 }
