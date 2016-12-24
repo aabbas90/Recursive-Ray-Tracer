212d3217009cf1986f6926518cd058f1af6da7ff
Chitra Singh <chits.26@gmail.com>
2016-12-24 21:57:54 +0100

    Update: triangle mapping

--- rt/coordmappers/tmapper.cpp
+++ rt/coordmappers/tmapper.cpp
@@ -4,15 +4,12 @@
 namespace rt
 {
     TriangleMapper::TriangleMapper(const Point& tv0, const Point& tv1, const Point& tv2)
-    {
-        
-    }
+    : tv0(tv0), tv1(tv1), tv2(tv2){}
     TriangleMapper::TriangleMapper(Point ntv[3])
-    {
-
-    }
+    : tv0(ntv[0]), tv1(ntv[1]), tv2(ntv[2]){}
     Point TriangleMapper::getCoords(const Intersection& hit) const
     {
-        return Point();
+        Point hitPoint = hit.local();
+        return Point (tv0 * (1 - (hitPoint.x + hitPoint.y)) + tv1 * hitPoint.x + tv2 * hitPoint.y);
     }
 }
--- rt/coordmappers/tmapper.h
+++ rt/coordmappers/tmapper.h
@@ -12,7 +12,8 @@ public:
     TriangleMapper(const Point& tv0, const Point& tv1, const Point& tv2);
     TriangleMapper(Point ntv[3]);
     virtual Point getCoords(const Intersection& hit) const;
-
+private:
+    Point tv0, tv1, tv2;
 };
 
 }
