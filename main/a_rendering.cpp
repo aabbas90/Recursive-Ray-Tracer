47265a148d25f12b8b578528c02d37d282d7c90a
Chitra Singh <chits.26@gmail.com>
2017-01-28 15:29:35 +0100

    Update: apply material directly to scene using mtl object name

--- main/a_rendering.cpp
+++ main/a_rendering.cpp
@@ -14,15 +14,35 @@
 #include <rt/lights/directional.h>
 #include <rt/primmod/instance.h>
 #include <core/matrix.h>
+#include <rt/materials/lambertian.h>
+#include <rt/textures/constant.h>
+#include <rt/textures/imagetex.h>
+
+#include <rt/materials/flatmaterial.h>
+#include <rt/materials/phong.h>
+#include <rt/materials/mirror.h>
+#include <rt/materials/combine.h>
+#include <rt/materials/cookTorrance.h>
+
+#include <rt/integrators/recraytrace.h>
+
 
 using namespace rt;
 void a_rendering()
 {
     Image img(800, 600);
-	float xRotation = -15;
-    float yRotation = -40;
+	// float xRotation = -6;
+    // float yRotation = -70;
+    // float zRotation = 0;
+    // Point cameraPostion = Point(-400, 90, 100);
+    // Vector upVector = Vector(0.0f, 1.0f, 0.0f);
+    // Vector forwardVector = Vector(0.0f, 0.0f, -1.0f);
+
+
+    float xRotation = 0;
+    float yRotation = 0;
     float zRotation = 0;
-    Point cameraPostion = Point(-500, 200, 400);
+    Point cameraPostion = Point(0, 0, 12);
     Vector upVector = Vector(0.0f, 1.0f, 0.0f);
     Vector forwardVector = Vector(0.0f, 0.0f, -1.0f);
 
@@ -37,9 +57,27 @@ void a_rendering()
     upVector = rotationMatrix * upVector;
     forwardVector = rotationMatrix * forwardVector;
 
+    Texture* greentex = new ConstantTexture(RGBColor(0.f,.7f,0.f));
+    Texture* bluetex = new ConstantTexture(RGBColor(0.f,0.f,0.7f));
+
+	Material* mat_stones = new PhongMaterial(greentex, 20.0f);
+    // Material* mat_stones = new LambertianMaterial(greentex, bluetex);
+    // Material* mat_stones = new MirrorMaterial(0.0f, 0.0f);
+
+    MatLib* matlib = new MatLib;
+    matlib->insert(std::pair<std::string, Material*>("sph_mat", mat_stones)); 
+    // matlib->insert(std::pair<std::string, Material*>("blinn1SG", mat_stones)); 
+
 
     BVH* scene = new BVH(false);
-    loadOBJ(scene, "models/", "scene1.obj");
+    loadOBJ(scene, "models/", "1_sph.obj", matlib);
+
+
+    // BVH* stageTopLight = new BVH(false);
+    // loadOBJ(stageTopLight, "models/", "1_light.obj");
+    // stageTopLight->translate(Vector(85.0f,0.0f,-27));
+    // scene->add(stageTopLight);
+
     scene->rebuildIndex();
     World world;
 	world.scene = scene;
@@ -49,10 +87,11 @@ void a_rendering()
     world.light.push_back(&dirl);
 
 	// PerspectiveCamera cam1(Point(0.0f, 5.0f, 30.0f), Vector(0.0f, 0.0f, -1.0f), Vector(0.0f, 1.0f, 0.0f), pi / 4, pi / 3);
+    // PerspectiveCamera cam1(cameraPostion, forwardVector, upVector, pi / 4, pi / 3);
     PerspectiveCamera cam1(cameraPostion, forwardVector, upVector, pi / 4, pi / 3);
 
-    RayCastingIntegrator integrator(&world);
+    RecursiveRayTracingIntegrator integrator(&world);
     Renderer engine1(&cam1, &integrator);
     engine1.render(img);
-    img.writePNG("scene1.png");
+    img.writePNG("1_sph.png");
 }
